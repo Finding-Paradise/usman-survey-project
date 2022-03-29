b1bc8aa9c54fd1709aa8babd8f0366253892dceb
Ariet <nasirdinov.ari@gmail.com>
2022-03-30 00:16:06 +0600

    added content to quiz start page

--- lib/pages/survey_start_page.dart
+++ lib/pages/survey_start_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SurveyStartPage extends StatefulWidget {
   const SurveyStartPage({Key? key}) : super(key: key);
@@ -18,14 +19,77 @@ class _SurveyStartPageState extends State<SurveyStartPage> {
         child: Column(
           children: [
             Row(
-              children: const [
-                Expanded(
-                  child: Text('Basic Programming'),
+              children: [
+                SizedBox(
+                  width: 300,
+                  child: Text(
+                    'Basis Programming',
+                    style: GoogleFonts.montserrat(
+                      textStyle: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.w600),
+                    ),
+                  ),
                 ),
               ],
+              mainAxisAlignment: MainAxisAlignment.center,
             ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 300,
+                  child: Text(
+                    'lorem ' * 75,
+                    style: GoogleFonts.montserrat(
+                      textStyle: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 325,
+                  child: TextButton(
+                    child: Text(
+                      'Start',
+                      style: GoogleFonts.montserrat(
+                        textStyle: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                        const EdgeInsets.symmetric(vertical: 28.0),
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                      ),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 254, 122, 61),
+                      ),
+                      foregroundColor: MaterialStateProperty.all(Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            )
           ],
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
         ),
+      ),
+      bottomNavigationBar: const BottomAppBar(
+        color: Colors.blue,
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add),
       ),
     );
   }
