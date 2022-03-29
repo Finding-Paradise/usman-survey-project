185674677b1c17ea7e185c8bdcb08c50b77cfa4e
Ariet <nasirdinov.ari@gmail.com>
2022-03-29 22:26:06 +0600

    added page where user can start selected quiz

---
+++ lib/pages/survey_start_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SurveyStartPage extends StatefulWidget {
+  const SurveyStartPage({Key? key}) : super(key: key);
+
+  @override
+  State<SurveyStartPage> createState() => _SurveyStartPageState();
+}
+
+class _SurveyStartPageState extends State<SurveyStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
